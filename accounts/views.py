79439e4da9a7d495fa33cc405a5b8a6c47f09eed
qkrwlgus718 <qkrwlgus718@naver.com>
2023-04-20 17:33:34 +0900

    form

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
-from .forms import CustomUserCreationForm
+# from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm, LoginForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -12,12 +12,12 @@ def login(request):
         return redirect('posts:index')
 
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
+        form = LoginForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
             return redirect('posts:index')
     else:
-        form = AuthenticationForm()
+        form = LoginForm()
 
     context = {
         'form': form,
