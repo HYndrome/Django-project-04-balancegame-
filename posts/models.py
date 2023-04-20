d091cd40e42bed7118d7b28c7360dd5f796e2c55
SungBum Ha <gktjd425@naver.com>
2023-04-20 10:16:48 +0900

    basic_features

--- posts/models.py
+++ posts/models.py
@@ -5,9 +5,9 @@ from django.conf import settings
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    select1_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='select1')
+    select1_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='selects1')
     select1_content = models.CharField(max_length=50)
-    select2_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='select2')
+    select2_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='selects2')
     select2_content = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def create(request):
@@ -20,3 +25,23 @@ def create(request):
         'form': form,
     }
     return render(request, 'posts/create.html', context)
+
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/detail.html', context)
+
+
+def answer(request, post_pk, answer):
+    post = Post.objects.get(pk=post_pk)
+    if (request.user in post.select1_users.all()) or (request.user in post.select2_users.all()):
+        return redirect('posts:detail', post_pk)
+    else:
+        if answer == 1:
+            post.select1_users.add(request.user)
+        elif answer == 2:
+            post.select2_users.add(request.user)
+        return redirect('posts:detail', post_pk)
