0f012a7477017f8c33671e5538249b9d46240d96
HYndrome <hyndrome@gmail.com>
2023-04-21 16:49:27 +0900

    add setting.py ALLOWED_HOST

--- balancegame/settings.py
+++ balancegame/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-django-project-04-balancegame-e9btb72blgq8xbrz.sel3.cloudtype.app']
 
 
 # Application definition
